4526da7dbf6764405ff3db66acef9b9c09fe195a
JewelHFahim <jewelhfahim@gmail.com>
2023-06-06 22:24:10 +0600

    done

--- src/app.ts
+++ src/app.ts
@@ -2,14 +2,18 @@ import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
 const app: Application = express()
 
+import userRouter from './app/modules/users/users.route'
+
 app.use(cors())
 
 // parser
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Succesfull..!')
+app.use('/api/v1/users/', userRouter)
+
+app.get('/', async (req: Request, res: Response) => {
+  res.send('Server Working Succesfully..!')
 })
 
 export default app
